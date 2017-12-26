e5cb5894026d474344f7bffa53f168d9ca2b39af
Shibu Pillai <shibup082@gmail.com>
2017-12-27 00:14:14 +0530

    My Project Setup Completed Successfully.

---
+++ tdgbackend/src/main/java/com/tdg/tdgbackend/App.java
@@ -0,0 +1,13 @@
+package com.tdg.tdgbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
